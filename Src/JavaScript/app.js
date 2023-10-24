be517ecd350180288229a02b097a41c542d18a3e
Ali Azizi <aliazizi.4973@gmail.com>
2023-10-24 11:43:55 +0330

    Selected Element's

--- Src/JavaScript/app.js
+++ Src/JavaScript/app.js
@@ -1,1 +1,16 @@
 // todo: Get Start Code JavaScript App-Js
+// ? Selected input's Element HTML DOM :-
+let $ = document;
+const textName = $.querySelector(".input-name");
+const textFamily = $.querySelector(".input-family");
+const textPhone = $.querySelector(".input-phone");
+const textEmail = $.querySelector(".input-email");
+const textNumber = $.querySelector("input-number");
+const textDate = $.querySelector(".input-date");
+// todo Selected (select : City and Country) Element HTML DOM :-
+const selectCountry = $.getElementById("country");
+const selectCity = $.getElementById("city");
+// todo Selected Button's Element HTML DOM :-
+const btnTicketSales = $.querySelector(".btn-ticket-Sales");
+const btnClearAll = $.querySelector(".btn-clearAll");
+
