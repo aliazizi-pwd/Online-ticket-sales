ead28be24e3c44e87dea8623e07a55e03f025669
Ali Azizi <aliazizi.4973@gmail.com>
2023-10-24 13:08:49 +0330

    Codeing Part Prices Country

--- Src/JavaScript/app.js
+++ Src/JavaScript/app.js
@@ -4,9 +4,11 @@ let $ = document;
 const textName = $.querySelector(".input-name");
 const textFamily = $.querySelector(".input-family");
 const textPhone = $.querySelector(".input-phone");
-const textEmail = $.querySelector(".input-email");
 const textNumber = $.querySelector("input-number");
-const textDate = $.querySelector(".input-date");
+const textEmail = $.querySelector(".input-email");
+const textPrice = $.querySelector(".input-price");
+const textDateWent = $.querySelector(".input-Date-Went");
+const textDateReturn = $.querySelector(".input-Date-Return");
 // todo Selected (select : City and Country) Element HTML DOM :-
 const selectCountry = $.getElementById("country");
 const selectCity = $.getElementById("city");
@@ -24,13 +26,24 @@ const country = {
      France : ['Paris','Nice','Lyon','Lille','Marseille','Bordeaux'],
 };
 
+const priceCountry = {
+     Usa : ['1000'],
+     Iran : ['450'],
+     Turkey : ['750'],
+     Canada : ['150'],
+     Italy : ['950'],
+     France : ['850'],
+};
 
 selectCountry.addEventListener("change" , () => {
      let valueChange = selectCountry.value;
      let dataBaseValues = country[valueChange];
-     
+          
      selectCity.innerHTML = '';
      dataBaseValues.forEach((city) => {
           selectCity.innerHTML += `<option>${city}</option>`;
      });
+
+     textPrice.value = `${priceCountry[valueChange]}$`;
+
 });
